ff690c6a9a0130456e0e727883fe9a751cb9db87
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-30 22:03:13 -0300

    Uploado inicial

---
+++ AppContatos/src/test/java/br/com/ro/AppContatos/AppContatos/AppContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ro.AppContatos.AppContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
