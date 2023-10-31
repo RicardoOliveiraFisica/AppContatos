b353498ca55b910245a290166a64f033dd585e9f
RicardoOliveiraFisica <ric_fisica@yahoo.com.br>
2023-10-31 11:40:04 -0300

    criacao de models e repositories

---
+++ AppContatos/src/main/java/br/com/ro/AppContatos/AppContatos/model/Contato.java
@@ -0,0 +1,96 @@
+package br.com.ro.AppContatos.AppContatos.model;
+
+
+import java.util.Objects;
+
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contatos")
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pessoa_id", referencedColumnName = "id")
+	private Pessoa pessoa;	
+	
+	@Column(nullable = false)
+	@Range(min = 0, max = 1)
+	private Integer tipoContato;
+	
+	@Column(nullable = false)
+	private String contato;
+
+	public Contato(Long id, Pessoa pessoa, @Range(min = 0, max = 1) Integer tipoContato, String contato) {
+		this.id = id;
+		this.pessoa = pessoa;
+		this.tipoContato = tipoContato;
+		this.contato = contato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Integer getTipoContato() {
+		return tipoContato;
+	}
+
+	public void setTipoContato(Integer tipoContato) {
+		this.tipoContato = tipoContato;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+}
